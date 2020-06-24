28db819ec615f9a59126632c6707390beb16b761
908115501 <908115501@qq.com>
2020-06-25 00:32:00 +0800

    create GameManageService

---
+++ GameManageService/GameManageServiceUtil.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Controllers;
+using DataAccessAPI.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Ubiety.Dns.Core;
+
+namespace GameManageService
+{
+    class GameManageServiceUtil
+    {
+        TagsMapper tagsMapper = new TagsMapper();
+        //getAllTags
+        public Response getAllTags()
+        {
+            Response response = new Response();
+            ArrayList result = tagsMapper.SelectAll();
+
+            response.result = result;
+            response.status = "200";
+            return response;
+        }
+    }
+}
---
+++ GameManageService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameManageService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManageServiceUtil gameManageServiceUtil = new GameManageServiceUtil();
+            var result = gameManageServiceUtil.getAllTags();
+            Console.WriteLine(result.status);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

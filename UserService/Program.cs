fae63a7c80b1f495a406a18ea3e8090a3951a8a5
LudwigLiu-CN <17623857792@163.com>
2020-06-24 20:43:18 +0800

    login

---
+++ UserService/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessAPI.Models;
+using System;
+
+namespace UserService
+{
+    class Program
+    {
+        static UserServiceUtil userServiceUtil = new UserServiceUtil();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Users user = new Users();
+            user.UserName = "DYT";
+            user.Pwd = "123456";
+            int a = userServiceUtil.Login(user);
+        }
+    }
+}
---
+++ UserService/UserServiceUtil.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Controllers;
+using DataAccessAPI.Models;
+using Org.BouncyCastle.Asn1.X509;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService
+{
+    class UserServiceUtil
+    {
+        UserMapper usersMapper = new UserMapper();
+        public int Login(Users user)
+        {
+            ArrayList targets = usersMapper.SelectByUserName(user.UserName);
+            Users target = (Users)targets[0];
+            if (target.UserName == user.UserName && target.Pwd == user.Pwd)
+            {
+                return target.UserId;
+            }
+            return -1;
+        }
+    }
+}

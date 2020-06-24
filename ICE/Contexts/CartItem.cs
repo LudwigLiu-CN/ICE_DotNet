2c7a37b6d7fff2aab1076245c90746b996c8e7f4
budi <budi@bodideMacBook-Pro.local>
2020-06-24 17:17:04 +0800

    budi's Mapper Update

---
+++ ICE/Contexts/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contexts
+{
+    public class CartItem
+    {
+        public int GameId { get; set; }
+        public String title { get; set; }
+        public float? price { get; set; }
+        public String coverPath { get; set; }
+        public bool? discount { get; set; }
+        public String consoleName { get; set; }
+        public int consoleId { get; set; }
+        public int cateId { get; set; }
+        public String cateName { get; set; }
+    }
+}

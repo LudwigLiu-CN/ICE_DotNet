8948d13fa0891304c3a8ff93759a8ee23641213e
budi <budi@bodideMacBook-Pro.local>
2020-06-24 16:21:10 +0800

    budi's Mappers

---
+++ ICE/Controllers/ChartMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataAccess.Controllers
+{
+    public class ChartMapper : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

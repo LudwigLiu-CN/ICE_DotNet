3e10c457d23ad416b8d00e994525238e709f1ffb
908115501 <908115501@qq.com>
2020-06-24 21:30:22 +0800

    Create PublisherPageServiceUtil.cs

---
+++ PublisherPageController/PublisherPageServiceUtil.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublisherPageService
+{
+    class PublisherPageServiceUtil
+    {
+        PublisherMapper publisherMapper = new PublisherMapper();
+
+        
+    }
+}

ecedc4597fd2055b964e78f7ce619d15d5f163aa
LudwigLiu-CN <17623857792@163.com>
2020-06-25 15:39:44 +0800

    getMyCart

--- DataAccessAPI/Controllers/ChartMapper.cs
+++ DataAccessAPI/Controllers/ChartMapper.cs
@@ -43,26 +43,36 @@ namespace DataAccess.Controllers
 
         public ArrayList GetMyCart(int UserId, int StartWith, int EndWith, int Reverse)
         {
-            ConsolesMapper consolesMapper = new ConsolesMapper();
-            var targets = from c in iceContext_.Chart where c.UserId == UserId select c;
+            var targets = from c in iceContext_.Chart
+                          join g in iceContext_.Games on c.GameId equals g.GameId
+                          join console in iceContext_.Consoles on c.ConsoleId equals console.ConsoleId
+                          where c.UserId == UserId
+                          select new
+                          {
+                              game_id = g.GameId,
+                              title = g.Title,
+                              price = g.Price,
+                              coverPath = g.CoverPath,
+                              discount = g.Discount,
+                              consoleName = console.ConsoleName,
+                              consoleId = console.ConsoleId
+                          };
             ArrayList results = new ArrayList();
-            foreach(var c in targets)
+            foreach (var c in targets)
             {
                 CartItem cartItem = new CartItem();
-                Games game = iceContext_.Games.Find(c.GameId);
-                cartItem.GameId = game.GameId;
-                cartItem.title = game.Title;
-                cartItem.price = game.Price;
-                cartItem.coverPath = game.CoverPath;
-                cartItem.discount = game.Discount;
-                cartItem.consoleName = consolesMapper.SelectByPrimaryKey(c.ConsoleId).ConsoleName;
-                cartItem.consoleId = c.ConsoleId;
-                cartItem.cateId = c.Game.Belong.CateId;
-                cartItem.cateName = c.Game.Belong.Cate.CateName;
+                cartItem.GameId = c.game_id;
+                cartItem.title = c.title;
+                cartItem.price = c.price;
+                cartItem.coverPath = c.coverPath;
+                cartItem.discount = c.discount;
+                cartItem.consoleName = c.consoleName;
+                cartItem.consoleId = c.consoleId;
                 results.Add(cartItem);
             }
             return results;
         }
+
 
         public void AddToCart(int UserId, int GameId, int ConsoleId)
         {

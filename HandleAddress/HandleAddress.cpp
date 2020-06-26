10e94316d7c9e7d87d497baca077ccdc5c486b7c
908115501 <908115501@qq.com>
2020-06-26 20:33:00 +0800

    address helper

---
+++ HandleAddress/HandleAddress.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+#include "HandleAddress.h"
+
+namespace HandleAddress {
+	public ref class AddressHelper {
+	public:
+		static vector<string> separateAddresses(string Add) {
+			AddressHandler^ addressHandler = gcnew AddressHandler(Add);
+			addressHandler->separate();
+			return addressHandler->getSeparate();
+		}
+
+		static string concatnateAddresses(vector<string> AddList) {
+			AddressHandler^ addressHandler = gcnew AddressHandler(AddList);
+			addressHandler->concatenate();
+			return addressHandler->getAll();
+		}
+
+	};
+}
